1e74a2f25335d6dcb7e84e7ee81741ab1cb4f7ce
7ookie <ts_yovchev@mail.bg>
2017-07-18 15:26:11 +0300

    createExcelFile

---
+++ CalculateFactorial/ComputeFactorial.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace CalculateFactorial
+{
+	class ComputeFactorial
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Enter number. 1 <= number <= 1000 \nnumber = ");
+			int num = int.Parse(Console.ReadLine());
+			if (num < 0 || num > 1000)
+			{
+				Console.WriteLine("Wrong input.");
+				return;
+			}
+			Console.WriteLine();
+			Console.WriteLine("Calculate factorial");
+			Console.WriteLine("The result of {0}! is: {1}\n", num, CalculateFactorial(num));
+			Console.WriteLine("Calculate factorial optimized");
+			Console.WriteLine("The result of {0}! is: {1}\n", num, CalculateFactorialOptimized(num));
+		}
+		static BigInteger CalculateFactorial(int number)
+		{
+			BigInteger result = 1;
+			if (number == 0)
+			{
+				return 1;
+			}
+			Stopwatch stopwatch = new Stopwatch();
+			stopwatch.Start();
+			for (int i = 1; i <= number; i++)
+			{
+				result *= i;
+			}
+			stopwatch.Stop();
+			Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+			return result;
+		}
+
+		static BigInteger CalculateFactorialOptimized(int number)
+		{
+			BigInteger sum = number;
+			BigInteger result = number;
+			if (number == 0)
+			{
+				return 1;
+			}
+			Stopwatch stopwatch = new Stopwatch();
+			stopwatch.Start();
+			for (int i = number - 2; i > 1; i -= 2)
+			{
+				sum = (sum + i);
+				result *= sum;
+			}
+
+			if (number % 2 != 0)
+			{
+				result *= number / 2 + 1;
+			}
+			stopwatch.Stop();
+			Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+			return result;
+		}
+	}
+
+}
---
+++ ExcelFileGenerator/ExcelFileGenerator.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.Office.Interop.Excel;
+using System.IO;
+
+namespace ExcelFileGenerator
+{
+	public class ExcelFileGenerator
+	{
+		public static void Main(string[] args)
+		{
+			CreateTable();
+			CreateFile();
+			Console.WriteLine();
+		}
+
+		public static System.Data.DataTable CreateTable()
+		{
+			List<string> namesList = new List<string> { "Pavel", "Petar", "Tony", "Bob", "Joe", "Tod", "Ivan", "Pablo", "Kevin", "Kro" };
+			string name = string.Empty;
+			byte age = 0;
+			byte score = 0;
+			byte averageScore = 0;
+			string formula = "C2:C101";
+			Random rnd = new Random();
+
+			System.Data.DataTable table = new System.Data.DataTable();
+			table.Columns.Add("Name", typeof(string));
+			table.Columns.Add("age", typeof(byte));
+			table.Columns.Add("score", typeof(byte));
+			table.Columns.Add("averageScore", typeof(byte));
+			table.Columns.Add("formula", typeof(string));
+
+			//TODO make it work with 100k records
+			for (int i = 1; i < 100; i++)
+			{
+				byte rndName = (byte)rnd.Next(namesList.Count);
+				name = (string)namesList[rndName];
+				age = (byte)rnd.Next(20, 81);
+				score = (byte)rnd.Next(0, 101);
+				averageScore = (byte)(age + score);
+				table.Rows.Add(name, age, score, averageScore, formula);
+			}
+			return table;
+		}
+
+		public static void CreateFile()
+		{
+			Application excelApp = null;
+			Workbook workbook = null;
+			Worksheet sheet = null;
+			Range range = null;
+
+			try
+			{
+				string file = Path.Combine(Environment.CurrentDirectory, @"..\..\scores.xlsx");
+				excelApp = new Application();
+				workbook = excelApp.Workbooks.Add();
+				sheet = (Worksheet)workbook.Sheets[1];
+				sheet.Name = "scores";
+
+			
+				sheet.Cells[1, 1] = "Name";
+				sheet.Cells[1, 2] = "Age";
+				sheet.Cells[1, 3] = "Score";
+				sheet.Cells[1, 4] = "AverageScore";
+				sheet.Cells[1, 5] = "Formula";
+
+				range = sheet.Range[sheet.Cells[1, 1], sheet.Cells[1, 5]];
+				range.Interior.Color = XlRgbColor.rgbSkyBlue;
+				range.Font.Bold = true;
+
+				byte rowCounter = 2;
+				foreach (DataRow datarow in CreateTable().Rows)
+				{
+					for (int i = 0; i < CreateTable().Columns.Count; i++)
+					{
+						sheet.Cells[rowCounter, i + 1] = datarow.ItemArray[i];
+						if (rowCounter % 2 == 0)
+						{
+							range = sheet.Range[sheet.Cells[rowCounter, 1], sheet.Cells[rowCounter, 5]];
+							range.Font.Color = XlRgbColor.rgbGreen;
+						}
+					}
+					rowCounter += 1;
+				}
+
+				workbook.SaveAs(file);
+				workbook.Close();
+				excelApp.Quit();
+			}
+			catch (Exception exception)
+			{
+				Console.Write(exception);
+			}
+		}
+	}
+}
